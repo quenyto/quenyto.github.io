de7537c00b2411e6da5abbbbda9ff22dc5a0f2ae
Kenny Chang <kennychang@s-164-67-237-160.resnet.ucla.edu>
2018-09-13 00:06:11 -0700

    crack

--- crack.py
+++ crack.py
@@ -1,73 +1,48 @@
-def letter_to_number(s):
-  x = 0
-  for i in range(len(s)):
-    x *= 10
-    x += (ord(s[i]) - ord('a') + 1) % 10
-  return x
+import hashlib, getpass, time, base64, gc, Tkinter
 
-def strfy(n):
-  return str(n).zfill(4)[0:4]
+def d(n):
+    while n:
+        n, r = divmod(n - 1, 26)
+        yield r
 
-A = input("Last 4 digits of Vista phone number: ")
-B = input("Last 4 digits of serial number of 3/23/18 purchase in checking account: ")
-C = input("Last 4 digits of serial number of 7/05/18 purchase in checking account: ")
-D = input("Last 4 digits of Stanford Student ID: ")
-E = letter_to_number(raw_input("First letters of Stanford dorms for all four years (lowercase): "))
-key = raw_input("Insert key: ")
-K = [(pow(592589L, letter_to_number(key)*i, 730999993L)/10) % 10000 \
-    for i in range(5)]
-L = input("Password length: ")
+def a(n):
+  return ''.join(chr(ord('a') + p) for p in d(n))[::-1]
 
+def b(q):
+  return eval(base64.b64decode(q[::-1]))
 
-A_UPPERCASE = ord('A')
-ALPHABET_SIZE = 26
+Q = 'dZTNyEGaz5iYpxGazFGagwyJuQWZoNXaulmZg4WZodHISVEVOVEIzNXZyBlbc5iLuQmch9mYwlGbjBSZoRHIvRHIkVWaw92Yg4WZlJGIzFGagQmcvd3czFGUnACLnAiOpU2chNmcld3bshCI1ACanV3byhGdgEDIzVGZhJ3ZgwycyVGajFWZ0BiZvBycl1WYuBCdzFGbgUGa0BiZvBycyVGd0VGbgQ3cylmRnACLnAiOpgFWugFWYhFWkgCI05WZtVGdhR3cggTMvUDIu9GIlNmbhxWYiByZul2ajVGajByZulGZuV0JgwyJgoTKZlVWZ9CRE9STNhCIu9Wa0FmbpN2YhZHIhxGblNWayFmdgQmbvNWZzBiZvBSZ0FGRnACLnAiO5V2agQnclNnbJdyW'
 
+k = getpass.getpass(b(Q)[0])
+h = b(Q)[-1](k)
 
-def _decompose(number):
-    """Generate digits from `number` in base alphabet, least significants
-    bits first.
+for i in range(len(b(Q)) - 3):
+  h.update(getpass.getpass(b(Q)[i+1]))
+  gc.collect()
 
-    Since A is 1 rather than 0 in base alphabet, we are dealing with
-    `number - 1` at each iteration to be able to extract the proper digits.
-    """
+N = T = B = long(h.hexdigest(), 16)
+for i in range(9999):
+  h.update(k + str(pow(6733, i, 7011023)))
+  f = k + str(i) + h.hexdigest()
+  g = long(b(Q)[-1](f).hexdigest(), 16) ^ B
+  N = g % 10 if (i + g + N) % 10 == 0 else N
+  T = max(g, T)
+  B = min(g, B)
 
-    while number:
-        number, remainder = divmod(number - 1, ALPHABET_SIZE)
-        yield remainder
+L = len(k) * 4 - 14 
+pL = max(min(L, 22), 7)
+p = a((T ^ B) % (26 ** 20)).rjust(12, 'z')
+p = (p[0].upper() + p[1:(pL - 2)] + "." + str(N))[0:L]
+h = L = pL = T = B = 0
+gc.collect()
 
-
-def base_10_to_alphabet(number):
-    """Convert a decimal number to its base alphabet representation"""
-
-    return ''.join( \
-            chr(A_UPPERCASE + part) \
-            for part in _decompose(number) \
-    )[::-1]
-
-c = (letter_to_number(key) / 99) % 5
-
-Y = [strfy((A + K[(0 + c) % 5]) % 10000),\
-     strfy((B + K[(1 + c) % 5]) % 10000), \
-     strfy((C + K[(2 + c) % 5]) % 10000), \
-     strfy((D + K[(3 + c) % 5]) % 10000), \
-     strfy((E + K[(4 + c) % 5]) % 10000)]
-
-numstr = ""
-for i in range(4):
-  for j in range(5):
-    numstr += Y[j][i]
-
-encoded = pow(814267044343L, long(numstr), 95428942451557009L)
-pwd = base_10_to_alphabet(encoded).lower().rjust(12, 'z')
-
-pl = L
-if L < 7:
-  pl = 7
-if L > 14:
-  pl = 14
-
-pwd = pwd[:(pl - 3)] + str(len(key) % 10) + "@" + pwd[pl-3].upper()
- 
-print("Password is: " + pwd[:L])
-
-raw_input()
+r = Tkinter.Tk()
+r.withdraw()
+r.clipboard_clear()
+r.clipboard_append(p)
+raw_input(b(Q)[-2])
+r.clipboard_clear()
+r.clipboard_append(" ")
+r.update()
+time.sleep(0.5)
+r.destroy()
